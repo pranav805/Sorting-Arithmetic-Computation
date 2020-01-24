7b446859d122924d3b1c11937c5d86243107e16e
pranav805 <pranav.ige96@gmail.com>
2020-01-24 13:01:50 +0530

    ADDED: Stored the results in Dictionary

--- -T 3 SortingArithmaticComputation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-read -p "Enter the value of a: " a
-read -p "Enter the value of b: " b
-read -p "Enter the value of c: " c
-result1=`echo "scale=2; $a + $b * $c" | bc`
-result2=`echo "scale=2; $a * $b + $c" | bc`
-result3=`echo "scale=2; $c + $a / $b" | bc`
-result4=`echo "scale=2; $a % $b + $c" | bc`
--- SortingArithmaticComputation.sh
+++ SortingArithmaticComputation.sh
@@ -1,9 +1,17 @@
 #!/bin/bash -x
+
+declare -A ResultsDictionary
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
+
 result1=`echo "$a + $b * $c" | bc`
 result2=`echo "$a * $b + $c" | bc`
 result3=`echo "scale=2; $c + $a / $b" | bc`
 result4=`echo "$a % $b + $c" | bc`
+
+ResultsDictionary[Expression_1]=$result1
+ResultsDictionary[Expression_2]=$result2
+ResultsDictionary[Expression_3]=$result3
+ResultsDictionary[Expression_4]=$result4
 
