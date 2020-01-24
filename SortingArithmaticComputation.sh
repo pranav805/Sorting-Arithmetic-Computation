4a9b27eed32c69e22f0bafdfeacc4da6d46480c0
pranav805 <pranav.ige96@gmail.com>
2020-01-24 11:39:50 +0530

    ADDED: Equation=c + a / b, REFACTORED: Eq1 & Eq2

--- SortingArithmaticComputation.sh
+++ SortingArithmaticComputation.sh
@@ -2,6 +2,6 @@
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
-result1=$(($a + $b * $c))
-result2=$(($a * $b + $c))
-
+result1=`echo "scale=2; $a + $b * $c" | bc`
+result2=`echo "scale=2; $a * $b + $c" | bc`
+result3=`echo "scale=2; $c + $a / $b" | bc`
